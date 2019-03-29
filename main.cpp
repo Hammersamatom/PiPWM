e1863e3436dc2cbf00e88001aafa615ffec09329
Hammersamatom <hammersamatom@gmail.com>
2019-03-29 00:03:09 -0400

    Moved all Ints to Int16_t.

--- main.cpp
+++ main.cpp
@@ -14,24 +14,24 @@ using namespace std;
 #define PWMpin 0
 
 // These do not change. Scalar might be changeable to support different frequencies. 
-const int Scalar         = 100;
-const int tempHolderSize = 10;
+const int16_t Scalar         = 100;
+const int16_t tempHolderSize = 10;
 const char tempPath[]    = {"/sys/class/thermal/thermal_zone0/temp"};
 
 // Presetting some variables to avoid problems.
-int tempHolder[tempHolderSize] = {tempHolderSize};
-int runTemp                    = 0;
-int avgTemp                    = 0;
-int minTemp                    = 0;
-int maxTemp                    = 0;
-int avgRun                     = 0;
+int16_t tempHolder[tempHolderSize] = {tempHolderSize};
+int16_t runTemp                    = 0;
+int16_t avgTemp                    = 0;
+int16_t minTemp                    = 0;
+int16_t maxTemp                    = 0;
+int16_t avgRun                     = 0;
 
 
 
 void setPWM(float percentIn)
 {
-    int onTime  = percentIn * Scalar;
-    int offTime = Scalar - onTime;
+    int16_t onTime  = percentIn * Scalar;
+    int16_t offTime = Scalar - onTime;
 
     digitalWrite(PWMpin, 1);
     this_thread::sleep_for(chrono::milliseconds(onTime));
@@ -39,15 +39,15 @@ void setPWM(float percentIn)
     this_thread::sleep_for(chrono::milliseconds(offTime));
 }
 
-int avgTheTemp(int inVal)
+int16_t avgTheTemp(int16_t inVal)
 {
     // This number is set to 0, allows for numbers to be added and averaged 
-    int unDivNum = 0;
+    int16_t unDivNum = 0;
 
     // Presets the entire tempHolder array to the current inVal to gurantee unDivNum !< minTemp
     if (tempHolder[0] == tempHolderSize)
     {
-        for (int i = 0; i < tempHolderSize; i++)
+        for (int16_t i = 0; i < tempHolderSize; i++)
         {
             tempHolder[i] = inVal;
         }
@@ -60,12 +60,12 @@ int avgTheTemp(int inVal)
         avgRun = 0;
     }
 
-    for (int i = 0; i < tempHolderSize; i++)
+    for (int16_t i = 0; i < tempHolderSize; i++)
     {
         unDivNum += tempHolder[i];
     }
 
-    for (int i = 0; i < tempHolderSize; i++)
+    for (int16_t i = 0; i < tempHolderSize; i++)
     {
         printf("%d: %d\n", i, tempHolder[i]);
     }
@@ -73,7 +73,7 @@ int avgTheTemp(int inVal)
     return ceil(unDivNum/tempHolderSize);
 }
 
-int getTemp()
+int16_t getTemp()
 {
     char *val = new char[5];
 
@@ -88,7 +88,7 @@ int getTemp()
     temperatureFile.close();
 
     // The output of the /sys/ file is five char. We cast to float, divide until two decimal places. Round up, divide by ten to get correct value.
-    return atof(val)/100;
+    return atof(val)/10;
 }
 
 int main()
@@ -117,7 +117,7 @@ int main()
 
         avgTemp = avgTheTemp(runTemp);
 
-        int Difference = maxTemp - minTemp;
+        int16_t Difference = maxTemp - minTemp;
         float Percent  = (float)(avgTemp - minTemp)/Difference;
 
         printf("%f %d %d %d %d %d\n", Percent, avgTemp, runTemp, minTemp, maxTemp, Difference);
